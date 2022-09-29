cdbb65814f6d39c4d8cdc5ddd4662e5193311d10
shafik720 <shafikrasel5@gmail.com>
2022-09-30 00:55:44 +0600

    sfdsdg

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,12 @@
 
 const addBox = document.querySelector('.add-box'),
-popupBox = document.querySelector('.popup-box');
+popupBox = document.querySelector('.popup-box'),
+closeIcon = document.querySelector('.content header i')
+;
 
 addBox.addEventListener('click',()=>{
     popupBox.classList.add('show');
+})
+closeIcon.addEventListener('click',()=>{
+    popupBox.classList.remove('show');
 })
